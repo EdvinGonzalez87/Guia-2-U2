c37536f900ab623d30d7399c64bd72a108129529
EdvinGonzalez87 <agonzalezdb23@gmail.com>
2026-04-13 12:16:51 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Dias de la semana");
+       int numero= int.Parse(Console.ReadLine());   
+
+        switch (numero)
+        {
+            case 1:
+                Console.WriteLine("Lunes");   
+                break;      
+            case 2:
+                Console.WriteLine("Martes");
+                break;
+            case 3:
+                Console.WriteLine("Miercoles");
+                break;
+            case 4:
+                Console.WriteLine("Jueves");
+                break;
+            case 5:
+                Console.WriteLine("Viernes");
+                break;
+             case 6:
+                 Console.WriteLine("Sabado");
+                break;
+            case 7:
+                Console.WriteLine("Domingo");
+                break;
+            default:
+                Console.WriteLine("Ingrese un numero valido");  
+                break;
+        }
+    }
+}

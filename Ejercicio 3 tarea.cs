2686249975e236e6eeac4709780df9ebfd072580
EdvinGonzalez87 <agonzalezdb23@gmail.com>
2026-04-18 16:06:19 -0600

    Update Ejercicio 3 tarea.cs

--- Ejercicio 3 tarea.cs
+++ Ejercicio 3 tarea.cs
@@ -6,10 +6,10 @@
         double precio = 0, total;
 
         Console.WriteLine("===== ESTADIO =====");
-        Console.WriteLine("1 → Palco (Q300.00)");
-        Console.WriteLine("2 → Tribuna (Q100 - Q125)");
-        Console.WriteLine("3 → Preferencia (Q50 - Q75)");
-        Console.WriteLine("4 → Generales (Q30 - Q50)");
+        Console.WriteLine("1  Palco (Q300.00)");
+        Console.WriteLine("2  Tribuna (Q100 - Q125)");
+        Console.WriteLine("3  Preferencia (Q50 - Q75)");
+        Console.WriteLine("4  Generales (Q30 - Q50)");
         Console.Write("Seleccione el sector: ");
         sector = Convert.ToInt32(Console.ReadLine());
 

2952d1069b1822006d5049547198b7a1e565e306
EdvinGonzalez87 <agonzalezdb23@gmail.com>
2026-04-18 15:20:13 -0600

    Add files via upload

---
+++ Ejercicio 2 tarea.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un caracter:");
+        char c = Convert.ToChar(Console.ReadLine());
+
+        switch (c)
+        {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                Console.WriteLine("Es una vocal");
+                break;
+
+            default:
+                if (c >= '0' && c <= '9')
+                {
+                    Console.WriteLine("Es un digito");
+                }
+                else
+                {
+                    Console.WriteLine("No es vocal ni digito");
+                }
+                break;
+        }
+    }
+}
